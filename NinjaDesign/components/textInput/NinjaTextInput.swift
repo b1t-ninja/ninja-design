4480f05011798193480d59e0ee245cc9c67dff6a
b1t-ninja <nrueckner@outlook.de>
2025-02-14 11:42:27 +0100

    updated theme

--- NinjaDesign/components/textInput/NinjaTextInput.swift
+++ NinjaDesign/components/textInput/NinjaTextInput.swift
@@ -25,20 +25,54 @@ struct NinjaTextFieldStyle: TextFieldStyle {
   }
 }
 
+struct NinjaPasswordInput: View {
+  @Binding var value: String
+  var font: Font = .title3
+  var bg: Color = Color(red: 0, green: 0, blue: 0, opacity: 0)
+  var fg: Color = .foregroundDefault
+  var placeholder: String = "Type"
+  var icon: String = "eye"
+  @State private var showPassword: Bool = false
+  
+  var body: some View {
+    HStack {
+      if showPassword {
+        TextField(placeholder, text: $value)
+          .textFieldStyle(NinjaTextFieldStyle(bg: bg, fg: fg, font: font))
+          .disableAutocorrection(true)
+          .autocapitalization(.none)
+        NinjaIconButton(font: font, icon: showPassword ? "eye.slash" : "eye", onClick: {showPassword.toggle()}).frame(height: 20)
+      } else {
+        SecureField(placeholder, text: $value)
+          .disableAutocorrection(true)
+          .autocapitalization(.none)
+          .textFieldStyle(NinjaTextFieldStyle(bg: bg, fg: fg, font: font))
+        NinjaIconButton(font: font, icon: showPassword ? "eye.slash" : "eye", onClick: {showPassword.toggle()}).frame(height: 20)
+      }
+    }
+  }
+}
+
 struct NinjaTextInput: View {
   @Binding var value: String
   var font: Font = .title3
   var bg: Color = Color(red: 0, green: 0, blue: 0, opacity: 0)
   var fg: Color = .foregroundDefault
   var placeholder: String = "Type"
+  var disableAutocorrect: Bool = false
   
   var body: some View {
     TextField(placeholder, text: $value)
+      .disableAutocorrection(disableAutocorrect)
+      .autocapitalization(.none)
       .textFieldStyle(NinjaTextFieldStyle(bg: bg, fg: fg, font: font))
   }
 }
 
 #Preview {
   @Previewable @State var text = "nothing"
-  NinjaTextInput(value: $text)
+  VStack {
+    NinjaTextInput(value: $text)
+    NinjaPasswordInput(value: $text)
+  }
 }
